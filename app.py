13204d012cd3f38b393a6a7563306f13833ccea0
Mansea <6210742034@student.tu.ac.th>
2021-11-26 22:37:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ camera = cv2.VideoCapture(0)  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
-def gen_frames(name):  # generate frame by frame from camera
+def gen_frames():  # generate frame by frame from camera
     while True:
         # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
@@ -24,17 +24,14 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
     
-@app.route('/register', methods=["POST", "GET"])
+@app.route('/register')
 def register():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("video_feed", name=user))
     return render_template('register.html')
 
-@app.route('/video_feed/<name>')
-def video_feed(name):
+@app.route('/video_feed')
+def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(name), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 

f9b749ce497224798dfc4a4a55bcaa1cea32223d
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2021-11-27 01:36:31 +0700

    refactor identification's code

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, Response, request, sessions, url_for, redirect
+from faceIdentification import predict
 import cv2
 import os
-from faceIdentification import predict
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+camera.set(3,1920)
 
-
-camera = cv2.VideoCapture(1)  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
-
+HEIGHT = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+WIDTH = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+unknown_dir = os.path.join(BASE_DIR, "unknown")
+card_dir = os.path.join(BASE_DIR, "card")
+face_dir = os.path.join(BASE_DIR, "face")
 
 
 def main_frames():
     while True:
         success, frame = camera.read()  # read the camera frame
-        cv2.imwrite(os.path.join(BASE_DIR , 'unknown.jpg'), frame)
+
+        # card
+        cv2.imwrite(os.path.join(card_dir , 'card.jpg'), frame[0+20:HEIGHT-20, 0+20:int(40 * WIDTH // 100)-20])
+        # face
+        cv2.imwrite(os.path.join(face_dir , 'face.jpg'), frame[0+20:HEIGHT-20, int(40 * WIDTH // 100)+20: int(40 * WIDTH // 100) + WIDTH - int(40 * WIDTH // 100)-20])
+        
+        # Mask for CARD
+        cv2.rectangle(frame, (0+10, 0+10), (int(40 * WIDTH // 100)-10, HEIGHT-10), (255, 255, 255), 4)
+        # Mask for FACE
+        cv2.rectangle(frame, (int(40 * WIDTH // 100)+10, 0+10), (WIDTH-10, HEIGHT-10), (255, 255, 255), 4)
+        
         if not success:
             break
         else:            
@@ -28,9 +40,8 @@ def main_frames():
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-
 def identification():
-    full_file_path = os.path.join(BASE_DIR , 'unknown.jpg')
+    full_file_path = os.path.join(face_dir , 'face.jpg')
     predictions = predict(full_file_path, model_path="trained_knn_model.clf")
     names = []
     for name, (top, right, bottom, left) in predictions:
@@ -40,8 +51,6 @@ def identification():
 @app.route('/')
 def index():
     return render_template('index.html',  identify = [])
-    
-
 
 @app.route('/main_feed')
 def main_feed():

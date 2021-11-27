55b75abe1fa8d1d1283cfc95d21392fdf9b8b140
Mansea <6210742034@student.tu.ac.th>
2021-11-27 21:21:39 +0700

    add train and capture

--- register_route.py
+++ register_route.py
@@ -1,5 +1,6 @@
-from app import app, request, camera, cv2, render_template, Response
-
+import os
+from app import app, request, camera, cv2, render_template, Response, BASE_DIR, image_dir
+from train_data import train
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -17,13 +18,25 @@ def gen_frames():  # generate frame by frame from camera
 def register():
     return render_template('register.html')
 
+
 @app.route('/takeimage', methods = ['POST'])
 def takeimage():
     name = request.form['name']
-    print(name)
+    count = request.form['count']
+    path = "./images/"+name+"/"
+    try:
+        os.makedirs(path)
+    except:
+        pass
     _, frame = camera.read()
-    cv2.imwrite(f'{name}.jpg', frame)
+    cv2.imwrite(f'{path+name+count}.jpg', frame)
     return Response(status = 200)
+
+@app.route('/train_model', methods = ['POST'])
+def train_model():
+    print('Training')
+    train(image_dir, os.path.join(BASE_DIR, "trained_knn_model.clf"), n_neighbors=2)
+    return render_template('register.html', message='Training Complete')
 
 @app.route('/video_feed')
 def video_feed():
